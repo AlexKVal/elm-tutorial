009cf126965d311c8bc92ef4da04ba99d8ab530e
AlexKVal <alexkval@gmail.com>
2016-06-28 16:56:44 +0300

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path")
+
+module.exports = {
+  entry: {
+    app: [
+      './src/index.js'
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.(css|scss)$/, loaders: ['style', 'css'] },
+      { test: /\.html$/, loader: 'file?name=[name].[ext]', exclude: /node_modules/ },
+      { test: /\.elm$/, loader: 'elm-webpack', exclude: [/elm-stuff/, /node_modules/] },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' }
+    ],
+
+    noParse: /\.elm$/
+  },
+
+  devServer: {
+    inline: true,
+    stats: { colors: true }
+  }
+}
